9c5c96f1bf204badf2f31aa24e64ea1a12b776a3
Nicolas Alcantara <nicolasalcantara088@gmail.com>
2023-04-17 18:47:29 -0300

    add

--- aula14/src/controllers/homeController.js
+++ aula14/src/controllers/homeController.js
@@ -1,12 +1,3 @@
-const HomeModel = require("../models/HomeModel");
-
-HomeModel.create({
-  titulo: "Teste do titulo",
-  descricao: "Teste da descricao",
-})
-  .then((dados) => console.log(dados))
-  .catch((e) => console.log(e));
-
 exports.paginaInicial = (req, res) => {
   res.render("index");
   return;
